df2b014cce493c45ae7e2628480c5fcc6b8be2f6
Yashwanth Durseti <yashwanthdurseti@gmail.com>
2024-10-22 19:35:47 +0530

    serverserverserver

--- server/server.js
+++ server/server.js
@@ -1,16 +1,9 @@
-const express = require('express');
-const dotenv = require('dotenv');
+const path = require('path');
 
-dotenv.config();
+// Serve static files from the React app
+app.use(express.static(path.join(__dirname, '../client/build')));
 
-const app = express();
-const PORT = process.env.PORT || 5000;
-
-// A simple route to test if the server is running
-app.get('/', (req, res) => {
-    res.send('Hello World');
-});
-
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+// The "catchall" handler: for any request that doesn't match one above, send back the React app
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../client/build', 'index.html'));
 });
