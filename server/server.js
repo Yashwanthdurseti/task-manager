b2e51e13fb3d8f13018f4c404e8610929f54d38a
Yashwanth Durseti <yashwanthdurseti@gmail.com>
2024-10-22 19:30:05 +0530

    build updation

--- server/server.js
+++ server/server.js
@@ -31,7 +31,7 @@ mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTop
 app.use(cors());
 
 app.use(cors({
-    origin: 'https://task-manager-yashwanth-941d9764a925.herokuapp.com/', // Replace with your Heroku app URL
+    origin: 'https://task-manager-yashwanth-941d9764a925.herokuapp.com/api', // Replace with your Heroku app URL
     credentials: true,
 }));
 
