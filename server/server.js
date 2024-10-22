6f90ce2357330b2aa0e46e264d905013486334cd
Yashwanth Durseti <yashwanthdurseti@gmail.com>
2024-10-22 22:37:30 +0530

    servfgkdfsghjkdfsfkkldr

--- server/server.js
+++ server/server.js
@@ -2,8 +2,8 @@ const express = require('express');
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 const path = require('path');
-const authRoutes = require('./routes/authRoutes'); // Ensure this file exists
-const taskRoutes = require('./routes/taskRoutes'); // Ensure this file exists
+const authRoutes = require('./routes/authRoutes');
+const taskRoutes = require('./routes/taskRoutes');
 const cookieParser = require('cookie-parser');
 const session = require('express-session');
 const cors = require('cors');
@@ -13,9 +13,9 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-// Middleware
 app.use(express.json());
 app.use(cookieParser());
+
 app.use(session({
     secret: 'your_session_secret',
     resave: false,
@@ -27,11 +27,13 @@ mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTop
     .then(() => console.log('MongoDB connected'))
     .catch(err => console.log(err));
 
+// Add this line before your routes
+app.use(cors());
 
-app.use(cors({
-    origin: '*', // Use this for testing only, restrict later
-    credentials: true,
-}));
+// app.use(cors({
+//     origin: 'https://task-manager-yashwanth-941d9764a925.herokuapp.com/api', // Replace with your Heroku app URL
+//     credentials: true,
+// }));
 
 // Serve static files from the React app
 app.use(express.static(path.join(__dirname, '../client/build')));
@@ -40,14 +42,18 @@ app.use(express.static(path.join(__dirname, '../client/build')));
 app.use('/api/auth', authRoutes);
 app.use('/api/tasks', taskRoutes);
 
-// Serve index.html for all other routes
+// The "catchall" handler: for any request that doesn't match one above, send back the React app
+// app.get('*', (req, res) => {
+//     res.sendFile(path.join(__dirname, '../client/build', 'index.html'));
+// });
+
 app.get('*', (req, res) => {
-    const indexPath = path.resolve('../client/build', 'index.html');
+    console.log(`Catch-all route hit: ${req.url}`);
+    const indexPath = path.join(__dirname, '../client/build', 'index.html');
     res.sendFile(indexPath);
 });
 
-// Start the server
+
 app.listen(PORT, () => {
-    console.log("hello");
     console.log(`Server is running on port ${PORT}`);
 });
