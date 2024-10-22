ff68d27cf5aa4b889a619ecde882837bd404ee9a
Yashwanth Durseti <yashwanthdurseti@gmail.com>
2024-10-22 19:33:46 +0530

    serverserver

--- server/server.js
+++ server/server.js
@@ -1,50 +1,14 @@
 const express = require('express');
-const mongoose = require('mongoose');
 const dotenv = require('dotenv');
-const path = require('path');
-const authRoutes = require('./routes/authRoutes');
-const taskRoutes = require('./routes/taskRoutes');
-const cookieParser = require('cookie-parser');
-const session = require('express-session');
-const cors = require('cors');
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(express.json());
-app.use(cookieParser());
-
-app.use(session({
-    secret: 'your_session_secret',
-    resave: false,
-    saveUninitialized: true,
-}));
-
-// Connect to MongoDB
-mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
-    .then(() => console.log('MongoDB connected'))
-    .catch(err => console.log(err));
-
-// Add this line before your routes
-app.use(cors());
-
-app.use(cors({
-    origin: 'https://task-manager-yashwanth-941d9764a925.herokuapp.com/api', // Replace with your Heroku app URL
-    credentials: true,
-}));
-
-// Serve static files from the React app
-app.use(express.static(path.join(__dirname, '../client/build')));
-
-// Routes
-app.use('/api/auth', authRoutes);
-app.use('/api/tasks', taskRoutes);
-
-// The "catchall" handler: for any request that doesn't match one above, send back the React app
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '../client/build', 'index.html'));
+// A simple route to test if the server is running
+app.get('/', (req, res) => {
+    res.send('Hello World');
 });
 
 app.listen(PORT, () => {
